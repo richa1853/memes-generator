c029cbf308b6fa6d888e16be38fc4a7cc829df85
richa_raj <rsbgs01@gmail.com>
2023-11-02 14:11:07 +0530

    added

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+function Footer() {
+    return (
+        <footer>
+            <p>
+                need help?
+               Contact:85678342022
+            </p>
+        </footer>
+    );
+}
+
+export default Footer;
